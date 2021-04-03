85107785a86e37d2d05027f43fd907c2c98eb379
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-04-03 23:57:46 +0300

    square root problem 1

--- square-root-of-an-integer/problem_1.py
+++ square-root-of-an-integer/problem_1.py
@@ -28,7 +28,7 @@ def sqrt(number):
 
 
 print("Pass" if (3 == sqrt(9)) else "Fail")
-# print("Pass" if (0 == sqrt(0)) else "Fail")
-# print("Pass" if (4 == sqrt(16)) else "Fail")
-# print("Pass" if (1 == sqrt(1)) else "Fail")
-# print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
