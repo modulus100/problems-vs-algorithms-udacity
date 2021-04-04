8c0fcbcc3cbe7b5c1a7810ddd5884a7349d9950b
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-04-04 19:05:48 +0300

    insert and search for task 5

--- autocomplete-with-tries/problem_5.py
+++ autocomplete-with-tries/problem_5.py
@@ -1,8 +1,10 @@
-# from ipywidgets import widgets
-# from IPython.display import display
-# from ipywidgets import interact
-#
-#
+import string
+
+from ipywidgets import widgets
+from IPython.display import display
+from ipywidgets import interact
+
+
 # def f(prefix):
 #     if prefix != '':
 #         prefixNode = MyTrie.find(prefix)
@@ -12,50 +14,69 @@
 #             print(prefix + " not found")
 #     else:
 #         print('')
-# interact(f,prefix='');
-#
-#
-# # Represents a single node in the Trie
-# class TrieNode:
-#     def __init__(self):
-#         ## Initialize this node in the Trie
-#         pass
-#
-#     def insert(self, char):
-#         ## Add a child node in this Trie
-#         pass
-#
-#     def suffixes(self, suffix=''):
-#         pass
-#
-#
-# ## Recursive function that collects the suffix for
-# ## all complete words below this point
-# # Add a child node in this Trie
-#
-#
-# # The Trie itself containing the root node and insert/find functions
-# class Trie:
-#     def __init__(self):
-#         pass
-#
-#     # Initialize this Trie (add a root node)
-#
-#     def insert(self, word):
-#         pass
-#
-#     # Add a word to the Trie
-#
-#     def find(self, prefix):
-#         pass
-# # Find the Trie node that represents this prefix
-#
-#
-# MyTrie = Trie()
-# wordList = [
-#     "ant", "anthology", "antagonist", "antonym",
-#     "fun", "function", "factory",
-#     "trie", "trigger", "trigonometry", "tripod"
-# ]
-# for word in wordList:
-#     MyTrie.insert(word)
+# interact(f,prefix='')
+
+
+# Represents a single node in the Trie
+class TrieNode:
+    def __init__(self, char=''):
+        # Initialize this node in the Trie
+        self.children: dict[string, TrieNode] = {}
+        self.char: string = char
+
+    def insert(self, char):
+        # Add a child node in this Trie
+        self.children[char] = TrieNode(char)
+
+    def suffixes(self, suffix=''):
+        pass
+
+
+## Recursive function that collects the suffix for
+## all complete words below this point
+# Add a child node in this Trie
+
+
+# The Trie itself containing the root node and insert/find functions
+class Trie:
+    # Initialize this Trie (add a root node)
+    def __init__(self):
+        self.root = TrieNode()
+
+    # Add a word to the Trie
+    def insert(self, word: string):
+        children = self.root.children
+
+        for char in word:
+            if char not in children:
+                children[char] = TrieNode(char)
+            children = children[char].children
+
+    # Find the Trie node that represents this prefix
+    def find(self, prefix: string) -> TrieNode or None:
+        last_index = len(prefix) - 1
+        i = 0
+        node = self.root
+
+        for char in prefix:
+            if char not in node.children:
+                return None
+            if i == last_index:
+                return node
+            node = node.children[char]
+
+        return node
+
+
+
+myTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym",
+    "fun", "function", "factory",
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    myTrie.insert(word)
+
+node = myTrie.find("ant")
+print(node.char)
