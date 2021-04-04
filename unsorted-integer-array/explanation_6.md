45490cdf716e2174372237bb6c346dc2347d4aee
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-04-04 15:04:09 +0300

    solution for task 6 + tests

--- unsorted-integer-array/explanation_6.md
+++ unsorted-integer-array/explanation_6.md
@@ -1,2 +1,5 @@
-## Overview
+Just a single iteration to find min and max.  
+
+Time complexity: **O(n)**.   
+Space complexity: **O(1)**.
 
--- unsorted-integer-array/problem_6.py
+++ unsorted-integer-array/problem_6.py
@@ -1,9 +1,13 @@
 import random
+import sys
 
 
-def get_min_max(ints):
-    min = ints[0]
-    max = ints[0]
+def get_min_max(ints: list):
+    if len(ints) == 0:
+        raise Exception("cannot by empty")
+
+    min = sys.maxsize
+    max = -sys.maxsize
 
     for element in ints:
         if element < min:
@@ -14,7 +18,21 @@ def get_min_max(ints):
     return min, max
 
 
+print("Test usual cases")
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
 print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
+print("\nTest with negative numbers")
+l = [i for i in range(-10, 11)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print("Pass" if ((-10, 10) == get_min_max(l)) else "Fail")
+
+
+print("\nTest empty array")
+try:
+    get_min_max([])
+except Exception:
+    print("Pass")
